9897b335c0d6777049e967e86e4da3b601898272
Tahmid Hussain <TahmidHussain@Tahmids-Mini.cust.communityfibre.co.uk>
2022-03-22 02:58:49 +0000

    some minor changes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
+
 export default function Home() {
   return (
     <>
@@ -11,6 +12,9 @@ export default function Home() {
     <div>
       <h1 className={styles.title}>Homepage</h1>
       <p className={styles.text}>Here are some of the videos I've worked on:</p>
+      <div>
+      
+      </div>
       <p className={styles.text}>Below are some of the projects I've worked on:</p>
       <Link href="projects">
       <a className={styles.btn}>See my projects</a>
